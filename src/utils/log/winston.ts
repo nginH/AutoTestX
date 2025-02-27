a54f87f89cbecfbb36e61c5a53c2e9bcf3aff066
Harsh Anand <80211435+nginH@users.noreply.github.com>
2025-02-27 23:24:07 +0530

    Add Winston logger for enhanced logging functionality

---
+++ src/utils/log/winston.ts
@@ -0,0 +1,24 @@
+import * as winston from 'winston';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+    const colorizer = winston.format.colorize();
+    const coloredMessage = colorizer.colorize('info', message as string);
+    return `${timestamp} [${level}]: ${coloredMessage}`;
+});
+
+export const _logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'error',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        }),
+        customFormat
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'autoTestX.log' })
+    ],
+});
